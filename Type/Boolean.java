8ae16328641566b67adff156a3954c302e009b98
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-20 00:54:49 +0900

    Create Boolean.java

---
+++ Type/Boolean.java
@@ -0,0 +1,13 @@
+//불린
+boolean isSuccess = true;
+boolean isTest = false;
+
+//조건문에서 불 연산 활용
+int base = 180;
+int height = 185;
+boolean isTall = height > base;
+
+//isTall이 참일 때 조건문 실행
+if (isTall) {
+  System.out.println('키가 큽니다.');
+}
