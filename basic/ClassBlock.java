a64d265d2fa4fc81dd45ec6cc54c29e166f8995d
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-20 00:44:21 +0900

    Create ClassBlock.java

---
+++ basic/ClassBlock.java
@@ -0,0 +1,11 @@
+/* 클래스 블록 */
+public class Sample {
+  public static void main(String[] args) { /* 메소드 블록 */
+    System.out.println('Hello Java');
+  }
+}
+
+/* sttic - 클래스 메소드 선언 'Sample.메소드' 형태로 호출 가능*/
+/* void - 메소드의 리턴 타입. void는 리턴값이 없음을 의미*/
+/* String[] args - 메소드의 매개 변수. args 변수는 배열 자료형이라는 뜻.
+/*System.out.println()는 명령문(Statement). 자바 스크립트 처럼 ;으로 끝남.
