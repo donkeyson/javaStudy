41c53d402b2197227c615d5fd09463c9672793ce
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-20 00:52:27 +0900

    Create Number.java

---
+++ Type/Number.java
@@ -0,0 +1,15 @@
+//정수
+int age = 10;
+long countOfStar = 4231431525214L; //더 넓은 범위 표현. L접미사 붙임.
+
+//실수
+float pi = 3.14F; //접미사 F
+double morePi = 3.14159265358979323846; //더 넓은 범위 표현.
+
+//8진수와 16진수
+int octal = 023; //십진수 19. 0으로 시작.
+int hex = 0xC; //십진수 12. 0x로 시작.
+
+System.out.println(++i); //11
+System.out.println(i++); //11
+System.out.println(i); //12
