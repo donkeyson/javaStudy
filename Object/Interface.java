5dd655ade42e7d626a8398dff63efa93c8c54216
donkeyson <88279704+donkeyson@users.noreply.github.com>
2022-01-23 20:21:12 +0900

    Update Interface.java

--- Object/Interface.java
+++ Object/Interface.java
@@ -1,6 +1,17 @@
 //인터페이스
 interface Predator {
   String getFood();
+  
+  //디폴트 메서드
+  default void printFood() {
+    System.out.println('my food is %s\n', getFood());
+  }
+  
+  int LEG_COUNT = 4; //인터페이스 상수
+  
+  //스태틱 메서드 (인터페이스명.스태틱메서드명)
+  static int speed() {
+    return LEG_COUNT * 30
 }
 
 class Animal {
